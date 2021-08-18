7d0ce9c344fb5775e5e37a105bd48766b5c1c99e
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2021-08-18 04:32:07 +0000

    RMX1921: Add Dirac Blobs
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

---
+++ dirac/dirac.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX1921/dirac/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    vendor/realme/RMX1921/dirac/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX1921/dirac/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
